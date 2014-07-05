bf872cde01326f13be1a3c78cd9733e921e850ea
Vipan Kumar <vipan.webdeveloper@gmail.com>
2014-07-05 19:13:07 +0530

    added model and helpers

--- app/code/local/Maze/OneStepCheckout/Helper/Data.php
+++ app/code/local/Maze/OneStepCheckout/Helper/Data.php
@@ -1,6 +1,29 @@
 <?php 
 
+/**
+ * Maze_OneStepChecout_Data helper class
+ *
+ * PHP version 5.4
+ *
+ * @category  Maze
+ * @package   Maze_OneStepChecout
+ * @author    Vipan Kumar <vipan.webdeveloper@gmail.com>
+ * @copyright 2014 Vipan Kumar
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version   GIT: <1.1>
+ *
+ */
 
+/** Maze_OneStepChecout_Data
+ *
+ * @category Maze
+ * @package  Maze_OneStepCheckout
+ *
+ * @author  Vipan Kumar <vipan.webdeveloper@gmail.com>
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version Release: 1.1
+ */
+ 
 class Maze_OneStepChecout_Data extends Mage_Core_Helper_Abstract
 {
     public function oneStepCheckoutEnabled()
